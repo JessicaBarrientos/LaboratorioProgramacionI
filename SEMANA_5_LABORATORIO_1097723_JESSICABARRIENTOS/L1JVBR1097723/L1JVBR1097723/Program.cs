d25539027f6321c92660b08a0509f9ebfa3c2839
JessicaBarrientos <141690263+JessicaBarrientos@users.noreply.github.com>
2023-09-04 20:37:55 -0600

    Add files via upload

---
+++ SEMANA_5_LABORATORIO_1097723_JESSICABARRIENTOS/L1JVBR1097723/L1JVBR1097723/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+          Console.WriteLine("Hola mundo");
+          Console.WriteLine("Soy Jess");
+
+          Console.Write("Hola mundo ");
+          Console.Write("soy Jess");*/
+
+        /*comentarios programa*/
+        /*Console.ReadKey();*/
+
+        Console.WriteLine("Ingrese su nombre:");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("Soy " + Nombre);
+
+        Console.Write("Hola mundo");
+        Console.Write(" soy " + Nombre);
+        Console.ReadKey();
+
+    }
+}
---
+++ SEMANA_5_LABORATORIO_1097723_JESSICABARRIENTOS/T1JVBM1097723/T1JVBM1097723/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T1JVBM1097723L
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+
+            Console.WriteLine("Ingrese su nombre:");
+            string sNombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad:");
+            string sEdad = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carrera:");
+            string sCarrera = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su carné:");
+            string sCarne = Console.ReadLine();
+
+
+            Console.WriteLine("Mi segundo programa");
+            Console.WriteLine("Nombre: " + sNombre);
+            Console.WriteLine("Edad: " + sEdad);
+            Console.WriteLine("Carrera: " + sCarrera);
+            Console.WriteLine("Carné: " + sCarne);
+
+            Console.Write("Soy " + sNombre);
+            Console.Write(", tengo " + sEdad);
+            Console.WriteLine(" años y estudio la carrera de " + sCarrera);
+
+            Console.WriteLine("Mi número de carné es " + sCarne);
+
+            Console.ReadKey();
+        }
+    }
+}
