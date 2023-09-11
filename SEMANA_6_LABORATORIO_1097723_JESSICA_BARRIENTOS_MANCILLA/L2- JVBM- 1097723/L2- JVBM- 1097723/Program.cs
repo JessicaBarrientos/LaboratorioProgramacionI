c78a8254d0d399e74b1525acf668a53d4b7917b2
JessicaBarrientos <141690263+JessicaBarrientos@users.noreply.github.com>
2023-09-11 10:03:12 -0600

    Add files via upload

---
+++ SEMANA_6_LABORATORIO_1097723_JESSICA_BARRIENTOS_MANCILLA/L2- JVBM- 1097723/L2- JVBM- 1097723/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*if (1>2)
+        {
+            Console.WriteLine("Instrucción IF");
+        }
+        else
+        {
+            Console.WriteLine("Instrucción ELSE");
+        }
+
+        Console.ReadKey();*/
+
+        Console.WriteLine("Ejercicio 2");
+        int numero; //variable tipo entero
+        string entrada; //variable tipo cadena de texto
+
+        Console.WriteLine("Ingrese un número del 1 al 7: ");
+        entrada = Console.ReadLine();
+        numero = Int32.Parse(entrada);
+
+        if (numero >=1 && numero<=7)
+        {
+            if (numero==1)
+            {
+                Console.WriteLine("DÍA: LUNES");
+            }
+            else if (numero == 2)
+            {
+                Console.WriteLine("DÍA: MARTES");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("DíA: MIÉRCOLES");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("DÍA: JUEVES");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("DÍA: VIERNES");
+            }
+            else if(numero==6)
+            {
+                Console.WriteLine("DÍA: SÁBADO");
+            }
+            else if(numero == 7)
+            {
+                Console.WriteLine("DÍA: DOMINGO");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: El número a ingresar debe estar contenido entre 1 y 7");
+        }
+
+    }
+}
