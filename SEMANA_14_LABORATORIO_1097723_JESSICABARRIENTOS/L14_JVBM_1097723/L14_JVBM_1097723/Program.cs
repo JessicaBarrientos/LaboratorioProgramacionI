42501f58b65c9d0fd763e745a98b017ea17f771a
JessicaBarrientos <141690263+JessicaBarrientos@users.noreply.github.com>
2023-11-06 09:16:03 -0600

    Add files via upload

---
+++ SEMANA_14_LABORATORIO_1097723_JESSICABARRIENTOS/L14_JVBM_1097723/L14_JVBM_1097723/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Semana 14");
+
+        int[] vector = new int[7];
+        int valores = 0;
+        int Max = 0;
+        int Min = 0;
+        int P = 0;
+        int N = 0;
+        int continuar = 0;
+
+       
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("Ingresar 7 números: ");
+                valores = int.Parse(Console.ReadLine());
+                vector[i] = valores;
+
+                if (vector[i] > Max)
+                {
+                    Max = vector[i];
+                }
+
+                if (vector[i] < Min )
+                {
+                    Min = vector[i];
+                }
+            }
+
+        Console.WriteLine("Nota más alta: " + Max);
+        Console.WriteLine("Nota más baja: " + Min);
+
+        Console.ReadKey();
+
+
+    }
+}
